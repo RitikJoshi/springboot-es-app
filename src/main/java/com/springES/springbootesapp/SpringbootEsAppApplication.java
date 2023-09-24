0ac000fe9e0d88f0f777611b13bf357c4ebd6174
Ritik Joshi <ritik.joshi329@gmail.com>
2023-09-24 14:34:02 +0530

    first commit

---
+++ src/main/java/com/springES/springbootesapp/SpringbootEsAppApplication.java
@@ -0,0 +1,13 @@
+package com.springES.springbootesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEsAppApplication.class, args);
+	}
+
+}
